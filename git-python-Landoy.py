9af9c6defe5b9f11565bdc11b2c90d30782fbbdd
LandoyGeneElpie <g.landoyelpie@gmail.com>
2024-02-14 22:22:39 +0800

    My Personal Info added

---
+++ git-python-Landoy.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 14 22:20:16 2024
+
+@author: Gene Elpie L. landoy
+"""
+
+print ("Full Name: Gene Elpie L. Landoy")
+print ("Address: Laylay, Boac, Marinduque")
+print ("Birthday: 02-24-2004")
+
